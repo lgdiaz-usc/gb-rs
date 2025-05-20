67ce231348c0eec1e143ac4632f21549d6550533
lgdiaz-usc <liamdiaz41@gmail.com>
2025-05-20 15:09:42 -0400

    Implemented a generic function for writing battery backed ram to a save file

--- src/mappers/mapper.rs
+++ src/mappers/mapper.rs
@@ -1,3 +1,5 @@
+use std::{fs::File, io::{BufWriter, Seek, Write}, sync::mpsc::Receiver};
+
 use super::{MBC1, NoMBC};
 
 pub enum Mapper {
@@ -17,6 +19,22 @@ impl Mapper {
         match self {
             Self::NoMBC(mapper) => mapper.write(address, value),
             Self::MBC1(mapper) => mapper.write(address, value),
+        }
+    }
+}
+
+fn write_thread(mut file: BufWriter<File>, data_receiver: Receiver<(u8, u64)>) {
+    loop {
+        if let Ok((value, address)) = data_receiver.recv() {
+            if address != file.stream_position().unwrap() {
+                file.seek(std::io::SeekFrom::Start(address)).unwrap();
+            }
+
+            file.write(&[value]).unwrap();
+        }
+        else {
+            file.flush().unwrap();
+            return;
         }
     }
 }
